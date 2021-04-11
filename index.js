555c4a80704c2520a2f189738d291eb4e41c6626
Kritsana135 <61050135@kmitl.ac.th>
2021-04-11 10:22:28 +0700

    alow all cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 require("dotenv").config()
+const cors = require('cors')
 // connect db
 const mongoose = require("mongoose")
 mongoose.connect(process.env.mongodb_uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
+
 // image Model
 const imageScema = mongoose.Schema(
   {
@@ -18,6 +20,7 @@ const imageScema = mongoose.Schema(
 )
 const Image = mongoose.model("Image", imageScema)
 
+app.use(cors())
 app.use(express.json())
 app.use(
   express.urlencoded({
